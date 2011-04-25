dc2b436637b9a6eb94c64b1a94597e22dc60e348
TOMITA Hiroshi <tomita@tomita-MacPro.(none)>
2011-04-25 19:14:57 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Botti::Application.config.secret_token = '882371777eef939739e11fd074571f6d4c754e1a2b648eb111170595154cbc1cf5d9a15115b0867e5e1ab0258b09a92c6f66489b2efb7f0c889f14b00fab5eb1'
