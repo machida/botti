9cc2ba833f91bd329d7b957e4adcb9fc625e6aef
TOMITA Hiroshi <tomita@tomita-MacPro.(none)>
2011-04-29 22:17:49 +0900

    fix small bugs and bad looks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
   def show
     @user = User.find(params[:id])
-    @tweets = @user.tweets
+    @tweets = @user.tweets.order("updated_at DESC")
     @tweet = Tweet.new(:user_id=>@user.id,
                        :content=>"ぼっち飯なう")
   end
