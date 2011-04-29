937ae2818384ed80038814b9bee3af13264f2ca3
TOMITA Hiroshi <tomita@tomita-MacPro.(none)>
2011-04-29 22:29:24 +0900

    location does not work...

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,11 +3,13 @@ class Location < ActiveRecord::Base
   acts_as_mappable :default_units=>:kms, :default_formula=>:flat
   attr_accessible :lat, :lng
 
+  private
   def initialize(args)
     begin
       ll = Geokit::LatLng.normalize(args)
       p ll
-      lat = ll.lat; lng = ll.lng
+      self.lat = ll.lat; self.lng = ll.lng
+      self.save!
     rescue
       super
     end
