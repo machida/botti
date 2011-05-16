4fb85ac176445a97b0f5444fc2ae96f01741697c
tomykaira <tomykaira@gmail.com>
2011-05-17 01:13:24 +0900

    try to use apigee

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -24,8 +24,7 @@ class TweetsController < ApplicationController
           config.oauth_token = auth.token
           config.oauth_token_secret = auth.secret
           if ENV['APIGEE_TWITTER_API_ENDPOINT']
-            p "using API"
-            config.proxy = "http://" + ENV['APIGEE_TWITTER_API_ENDPOINT']
+            p config.proxy = "http://" + ENV['APIGEE_TWITTER_API_ENDPOINT']
           end
         end
 
