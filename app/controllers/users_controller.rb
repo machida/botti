d62eacc2334fdab11fc60eaf6e69ce40b9e93f5d
tomykaira <tomykaira@gmail.com>
2011-05-17 13:15:05 +0900

    now the arrays are in time order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     if @user.tweets.count > 0
       @friend_tweets << @user.tweets.order("updated_at DESC").first
     end
-    @friend_tweets.each do |t|
+    @friend_tweets.reverse.each do |t|
       @js_query = generate_js_string(t.user, t)
     end
   end
@@ -24,11 +24,13 @@ class UsersController < ApplicationController
       p tweet
       return
     end
+    link = (user == current_user) ? "" : "<a href=#{message_tweet_path(tweet)}>DM</a>"
+    p link
     %Q% googlemap_controller.addFriend({
 pos:new google.maps.LatLng(#{tweet.location.lat},#{tweet.location.lng}),
 name: "#{user.nickname}",
 image_url: "#{user.image_url}",
-message: "#{tweet.message}"
+message: "#{user.nickname}: #{tweet.content} #{link} (#{tweet.time})"
 }
 );% # you cannot remove these "#{}"
   end
@@ -40,6 +42,6 @@ message: "#{tweet.message}"
         ft << f.tweets.order("updated_at DESC").first
       end
     end
-    ft
+    ft.sort_by{|obj| obj.updated_at}
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -17,10 +17,6 @@ class Tweet < ActiveRecord::Base
     updated_at.in_time_zone("Tokyo").strftime("%H:%M %d日")
   end
 
-  def message
-    "#{user.nickname}: #{content} (#{time})"
-  end
-
   def self.remove_old_tweets
     self.delete_all(["updated_at < ?", 3.days.ago])
   end
